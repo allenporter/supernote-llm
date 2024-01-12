18adc3b50e152536bf6af066a139fa4ca720cc09
Allen Porter <allen.porter@gmail.com>
2024-01-12 22:33:01 +0000

    Extract supernote contents

--- README.md
+++ README.md
@@ -2,12 +2,22 @@
 
 Consuming contents of a Supernote Notebook for use with LLMs
 
+## Extract text
+
+Using supernote-tool:
+
+Populate `notes/` with supernote files that contain text. The text will be
+extracted into `texts/`
+```
+$ python3 supernote-llm
+```
+
 ## Development log
 
 Ideas to explore:
 - [x] python development environment
-- [ ] Local respoitory of .note files
-- [ ] Extract text (autorecg) into a local text dump
+- [x] Local respoitory of .note files
+- [x] Extract text (autorecg) into a local text dump
 - [ ] Create llama index dev environment
 - [ ] Index notes into llama index
 - [ ] Test queries against llama index
---
+++ extract-text/__main__.py
@@ -0,0 +1,38 @@
+"""Supernote LLM."""
+
+from pathlib import Path
+import logging
+import supernotelib as sn
+
+_LOGGER = logging.getLogger(__name__)
+
+NOTE_DIR = Path("notes/")
+TEXT_DIR = Path("texts/")
+POLICY = "loose"
+
+
+def main():
+    """Main function."""
+    logging.basicConfig(level=logging.INFO)
+    _LOGGER.info("Starting supernote-llm")
+    for note_file in NOTE_DIR.glob("*.note"):
+        notebook = sn.load_notebook(note_file, policy=POLICY)
+        total_pages = notebook.get_total_pages()
+        _LOGGER.info(f"Converting %s (%s pages)", note_file, total_pages)
+        converter = sn.converter.TextConverter(notebook)
+        for i in range(total_pages):
+            text = converter.convert(i)
+            if text is None:
+                _LOGGER.info(f"Skipping page %s/%s", i+1, total_pages)
+                continue
+            text_filename = TEXT_DIR / f"{note_file.stem}-{i+1}.txt"
+            _LOGGER.info(f"Writing page %s/%s as %s", i+1, total_pages, text_filename)
+            with open(text_filename, 'w', encoding='utf-8') as f:
+                f.write(text)
+  
+
+
+    print("Main")
+
+if __name__ == "__main__":
+    main();
